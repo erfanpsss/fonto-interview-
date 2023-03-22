d353054e8ebcd83a41bb244bcf7862138337bec6
erfanpsss <erfanpsss@hotmail.com>
2023-03-23 04:53:09 +1100

    Added AddPropertyForm component

---
+++ src/components/forms.js
@@ -0,0 +1,30 @@
+import { Post } from "../adapters/take-home/requests.js";
+import { ADD_PROPERTY } from "../adapters/take-home/urls.js";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+export const AddPropertyForm = () => {
+    const [address, setAddress] = useState();
+    const [valuation, setValuation] = useState();
+    const [response, setResponse] = useState();
+    const navigate = useNavigate();
+
+    const submitForm = () => {
+        Post(ADD_PROPERTY, {"address": address, "valuation": valuation}).then((response) => {
+            setResponse("Property added");
+            navigate("/");
+        }).catch((reason) => {
+            setResponse(reason);
+            navigate("/");
+        })
+    }
+
+    return (
+        <div className="add-property-form">
+            <p>{response}</p>
+            <input type="text" name="address" placeholder="Address" onChange={event => {setAddress(event.target.value)}}></input>
+            <input type="number" name="valuation" placeholder="valuation" onChange={event => {setValuation(event.target.value)}}></input>
+            <input type="submit" value="Submit" onClick={() => {submitForm()}}></input>
+        </div>
+    )
+}
