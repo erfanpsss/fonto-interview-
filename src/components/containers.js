46b2d1f8730352b3448e4b75c2d85c9259ea3360
erfanpsss <erfanpsss@hotmail.com>
2023-03-23 04:52:40 +1100

    Added property container

---
+++ src/components/containers.js
@@ -0,0 +1,9 @@
+export const PropertyContainer = ({address, valuation, created_at}) => {
+    return (
+        <div>
+            {address}
+            {valuation}
+            {created_at}
+        </div>
+    )
+}
