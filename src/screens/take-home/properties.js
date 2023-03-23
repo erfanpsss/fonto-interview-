a3b2cfdcd9fa2d63e0eb655de181134bef207dc9
erfanpsss <erfanpsss@hotmail.com>
2023-03-23 11:46:12 +1100

    Added isLoading state to PropertiesScreen

--- src/screens/take-home/properties.js
+++ src/screens/take-home/properties.js
@@ -7,21 +7,23 @@ import { Styles } from "../../styles/properties.css";
 
 export const PropertiesScreen = () => {
     const [propertiesData, setPropertiesData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const navigate = useNavigate();
     
     useEffect(() => {
+
         Get(GET_ALL_PROPERTIES).then((response) => {
             let data = [];
-            console.log(response.data.properties)
             for(let property of response.data.properties) {
                 if (!property.hidden) {
                     property.created_at = new Date(property.created_at).toLocaleString(undefined, { timeZone: navigator.timeZone });;
                     data.push(property);
                 }
             }
-            console.log(data);
+            setIsLoading(false);
             setPropertiesData(data);
         }).catch((error) => {
+            setIsLoading(false);
             window.alert(error);
         })
     }, []);
@@ -29,7 +31,6 @@ export const PropertiesScreen = () => {
     const calculateValuationSum = () => {
         let sum = 0;
         for (let property of propertiesData) {
-            console.log(property)
             sum += property.valuation;
         }
         return sum;
@@ -37,10 +38,11 @@ export const PropertiesScreen = () => {
 
     return (
         <div className="properties-box">
+            <p>{ isLoading && "Loading data ..." }</p>
             {propertiesData.map(property => (
                 <PropertyContainer key={property.id} address={property.address} valuation={property.valuation} created_at={property.created_at}/>
             ))}
-            <p>Total valuation: {calculateValuationSum()}</p>
+            <p>Total valuation: AUD {calculateValuationSum().toLocaleString()}</p>
             <button onClick={() => {navigate("/add-properties")}}>Add</button>
         </div>
     )
